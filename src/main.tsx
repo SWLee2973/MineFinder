eaaa16a3ef59c2190cf1034e73296076820b0cdd
SWLee2973 <kek2973@naver.com>
2024-02-07 17:40:45 +0900

    [chore] 프로젝트 스케폴딩

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MineFinder from './MineFinder.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MineFinder />
+  </React.StrictMode>,
+)
